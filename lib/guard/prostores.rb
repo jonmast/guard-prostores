42cde9499f362a16584db4e5efb230db7df0dc9e
onlineamateur <dmastboys@gmail.com>
2013-09-15 08:30:26 -0400

    Cleanup indention. Fix storename detection.

--- lib/guard/prostores.rb
+++ lib/guard/prostores.rb
@@ -3,37 +3,41 @@ require 'guard/guard'
 require 'guard/prostores/uploader'
 require 'guard/prostores/ftptls'
 module Guard
-    class ProStores < Guard
-        def notify(msg, img)
-            ::Guard::Notifier.notify(msg, {:title => "Guard::Prostores", :img => img})
-        end
-        def initialize(watchers = [], options = {})
-            super
-            @options = options
-        end
-        def start
-            @postTemplate = Uploader.new
-        end
-        def run_on_changes(paths)
-            UI.debug paths
-            paths.each do |path|
-                if path.end_with?('.tpl')
-                    exitstatus  = @postTemplate.push(path, @options)
-                    if exitstatus == true
-                        notify("Posted #{path}", :success)
-                    else
-                        notify("Failed to upload #{path}", :failed)
-                    end
-                elsif path.end_with?('.js') || path.end_with?('.css')
-                    ftp = Net::FTPTLS.new
-                    ftp.connect('myftp.prostores.com')
-                    ftp.login(@options[:user], @options[:pass])
-                    ftp.passive=true
-                    ftp.put(path, "store/#{path}")
-                    notify("Uploaded #{path}", :success)
-                    ftp.close()
-                end
-            end
-        end
+  class ProStores < Guard
+    def notify(msg, img)
+      ::Guard::Notifier.notify(msg, {:title => "Guard::Prostores", :img => img})
     end
+    def initialize(watchers = [], options = {})
+      super
+      @options = options
+    end
+    def start
+      @postTemplate = Uploader.new
+    end
+    def run_on_modifications(paths)
+      UI.debug paths
+      paths.each do |path|
+        if path.end_with?('.tpl')
+          exitstatus = @postTemplate.push(path, @options)
+          if exitstatus == true
+            notify("Posted #{path}", :success)
+          else
+            notify("Failed to upload #{path}", :failed)
+          end
+        else
+          ftp = Net::FTPTLS.new
+          ftp.connect('myftp.prostores.com')
+          ftp.login(@options[:user], @options[:pass])
+          ftp.passive=true
+          begin
+           ftp.put(path, "store/#{path}")
+           notify("Uploaded #{path}", :success)
+          rescue Exception => e
+           UI.debug e.message
+          end
+          ftp.close()
+        end
+      end
+    end
+  end
 end
--- lib/guard/prostores/uploader.rb
+++ lib/guard/prostores/uploader.rb
@@ -1,24 +1,31 @@
 require 'httpclient'
 module Guard
-    class ProStores < Guard
-        class Uploader < HTTPClient
-            def push(path, options)
-                status = post("https://store01.prostores.com/servlet/wholesomebookswholesale/Admin/AdminTimeoutLogon?name=#{options[:user]}&password=#{options[:pass]}&jsenabled=true") unless cookies.join.include?('ps_auth')
-                return false unless status.body.include?('SUCCESS')
-                templateFile = open(path)
-                post('https://store01.prostores.com/servlet/wholesomebookswholesale/Admin/TemplateEditSource',
-                          {
-                           :tpl => path.split('.')[0],
-                           :templateseturl => options[:templateUrl],
-                           :confirmed => 'false',
-                           :promptSave => 'false',
-                           :promptPublish => 'true',
-                           :promptUndo => 'false',
-                           :tsource => templateFile.read
-                          })
-                templateFile.close
-                return true
-            end
+  class ProStores < Guard
+    class Uploader < HTTPClient
+      def push(path, options)
+        status = post('https://mystore.prostores.com/CP/submit/',
+              action: 'login',
+              username: options[:user],
+              password: options[:pass],
+              submit: 'Sign In')
+        if status.header['location'][0] =~ /let\/(.+)\/Admin/ #grab the store name from the redirect
+          storename = $1
+        else
+          return false
         end
+        templateFile = open(path)
+        post('https://store01.prostores.com/servlet/#{storename}/Admin/TemplateEditSource',
+              :tpl => path.split('.')[0],
+              :templateseturl => options[:templateUrl],
+              :confirmed => 'false',
+              :promptSave => 'false',
+              :promptPublish => 'true',
+              :promptUndo => 'false',
+              :tsource => templateFile.read
+            )
+        templateFile.close
+        return true
+      end
     end
+  end
 end
